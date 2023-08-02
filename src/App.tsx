6e537f2a646e0cc18e04cfc2d73df2afb3d33d20
Morotem <nathan.hvx@live.fr>
2023-08-02 13:19:56 +0200

    working with constant visible webcam
    CA MARCHE MAIS AVEC LA CAM PRESENTE

--- src/App.tsx
+++ src/App.tsx
@@ -21,19 +21,20 @@ function App() {
   const [tagValue, setTagValue] = useState("");
   const [nextStepValue, setNextStepValue] = useState("");
   const [temperature, setTemperature] = useState(30);
-  
+
   const [isStreaming, setIsStreaming] = useState(false);
   const [conditionRespected, setConditionRespected] = useState(false);
   const [showCam, setShowCam] = useState(false);
 
   const videoRef = useRef<Webcam>(null);
   // const image = useRef<string | null>(null);
-  const [image, setImage] = useState<string | null>("");
-  
+  // const [image, setImage] = useState<string | null>("");
+  const image = useRef<string | null | undefined>(null)
+
   const intervalRef = useRef<number | undefined>(undefined);
   const [webcams, setWebCams] = useState<MediaDeviceInfo[]>();
   const [choosenCam, setChoosenCam] = useState<string>();
-  
+
 
 
   const handleChange = (_event: Event, newValue: number | number[]) => {
@@ -43,15 +44,15 @@ function App() {
   };
 
   //test recuperation parametre via URL
-  useEffect(()=>{
-    
+  useEffect(() => {
+
     // setUrlValue(searchParams.get('URL') ?? "")
     // setKeyValue(searchParams.get('KEY') ?? "")
     // setTagValue(searchParams.get('TAG') ?? "")
     // setTemperature(parseFloat(searchParams.get('TEMP') ?? "75"))
     // setNextStepValue(searchParams.get('REDIRECT') ?? "")
-    
-  },[])
+
+  }, [])
 
   //initialisation liste des camera
   useEffect(() => {
@@ -59,7 +60,7 @@ function App() {
       const tabCams: MediaDeviceInfo[] = result.filter((infos) => infos.kind === "videoinput")
       setWebCams(tabCams)
       setChoosenCam(tabCams[0].deviceId)
-      
+
     });
   }, [])
 
@@ -70,33 +71,46 @@ function App() {
         deviceId: { exact: choosenCam }
       }
     };
-   
+
     if (isStreaming && !conditionRespected && videoRef.current) {
       navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
         setStream(stream);
 
         videoRef.current!.video!.srcObject = stream;
+        
       });
     }
   }, [isStreaming, choosenCam]);
 
-   // Méthode pour capturer une image depuis le flux vidéo
-   const captureImage = () => {
+  // Méthode pour capturer une image depuis le flux vidéo
+  const captureImage = () => {
+
     const video = videoRef.current?.video!
-    const canvas : HTMLCanvasElement = document.createElement('canvas');
+    
+    const canvas: HTMLCanvasElement = document.createElement('canvas');
+
     canvas.width = video.videoWidth;
     canvas.height = video.videoHeight;
-    canvas.getContext('2d')!.drawImage(video, 0, 0, canvas.width, canvas.height);
-    const imageDataURL = canvas.toDataURL("image/png"); // Convertir l'image en base64
-    setImage(imageDataURL);
+    const context = canvas.getContext('2d');
+    if (context != null) {
+      context.drawImage(video, 0, 0, canvas.width, canvas.height);
+      const imageDataURL = canvas.toDataURL("image/png");
+      console.log(canvas.toDataURL()) // Convertir l'image en base64
+      image.current = imageDataURL
+    // console.log(videoRef.current)
+      //  image.current = videoRef.current?.getScreenshot();
+    //   console.log(image.current)
+    }
+    
+
   };
 
   useEffect(() => {
- 
+    if(!isStreaming) return;
     intervalRef.current = setInterval(() => {
       captureImage();
-      if(isStreaming) checkVideo();
-      
+      checkVideo();
+
     }, 500);
 
     // Nettoyer l'intervalle lorsque le composant est démonté
@@ -153,7 +167,7 @@ function App() {
   //fonction pour revenir au menu
   const backRoot = () => {
     setIsStreaming(false);
-    setImage(null);
+    image.current = null;
     setConditionRespected(false);
     setShowCam(false);
   };
@@ -161,26 +175,26 @@ function App() {
   //fonction pour envoyer l'image à l'API
   const checkVideo = async () => {
     setIsStreaming(true);
-    console.log(image, conditionRespected)
-    if (image && conditionRespected === false) {
+
+    if (image.current && conditionRespected === false) {
       console.log("JE RENTRE DANS LE POST")
-      
+      console.log(image.current)
       const response = await fetch(urlValue, {
         method: "Post",
         headers: {
           "Prediction-Key": keyValue,
           "Content-Type": "application/octet-stream",
         },
-        body: dataUrlToFile(image),
+        body: dataUrlToFile(image.current!),
       });
       const data = await response.json();
+      console.log(data)
       data.predictions.forEach((prediction: Prediction) => {
         if (
           prediction.probability > temperature / 100 &&
           prediction.tagName == tagValue
         ) {
           setConditionRespected(true);
-        } else {
         }
       });
     }
@@ -199,8 +213,9 @@ function App() {
         height={720}
         width={1280}
         ref={videoRef}
-        hidden={!showCam}
+        
       />
+      <img src={image.current!} hidden={!showCam}></img>
       {!isStreaming ? (
         <>
           <Header />
@@ -251,15 +266,15 @@ function App() {
             label="Webcam"
             onChange={handleSelectCamChange}
           >
-            {webcams?.map((camInfo) =>{
+            {webcams?.map((camInfo) => {
               return <MenuItem key={camInfo.deviceId} value={camInfo.deviceId}>{camInfo.label}</MenuItem>
             })}
-            
+
           </Select>
         </>
       )}
 
-      {image && isStreaming ? (
+      {image.current && isStreaming ? (
         <>
           {conditionRespected ? (
             <h1>
