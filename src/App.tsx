d45e9c2897e44ca2b3401c10608d75db1e09cf30
Antoine Smet <antoinesmetdev@gmail.com>
2023-08-03 12:09:38 +0200

    add react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import "./App.css";
 import Header from "./component/Header";
 import CheckURL from "./component/CheckURL";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Form from "./component/Form";
 
 function App() {
-  
-
   return (
     <div className="App">
       <Header />
-      <CheckURL />
+      <Router>
+        <Routes>
+          <Route path="/setting" element={<CheckURL />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
